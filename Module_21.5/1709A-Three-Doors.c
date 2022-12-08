7bc6d72248f0c6273f14104ca92ef64f0253b4e0
musfiqur172 <musfiqur.rahman172@gmail.com>
2022-12-08 18:12:30 +0600

    new module added

---
+++ Module_21.5/1709A-Three-Doors.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+    {
+        }
+
+    return 0;
+}
